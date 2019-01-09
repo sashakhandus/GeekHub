45730300836283cee0fb92a235f83b2e042c6993
sashakhandus <sashakhandus@gmail.com>
2019-01-09 18:28:52 +0200

    add local storage

--- home13/src/js/main.js
+++ home13/src/js/main.js
@@ -1,17 +1,39 @@
 $(document).ready(function(){
 
+  localStorage.clear()
+
+  let oldValue;
+  let idLocalStorage;
+
   function reset() {
     $('.input-task').val('')
   }
 
-  function addTaskItem() {
-    let inputVal = $('.input-task').val()
+  function addTaskItem(inputVal) {
     let taskItem = $(`<li><input type="text" class="item-task" value=${inputVal}>
     <input type="button" class="input-delete" value="delete">
     <input type="button" class="input-edit" value="edit"></li>`)
-
     $('.task-list').append(taskItem)
   }
+
+  function addLocalStorage(inputVal) {
+    idLocalStorage ? idLocalStorage = localStorage.length : idLocalStorage = 0
+    console.log(idLocalStorage)
+    localStorage.setItem(idLocalStorage, inputVal)
+    idLocalStorage ++
+  }
+
+  function removeLocalStorage(inputVal) {
+    console.log(inputVal)
+    for(let i = 0, len = localStorage.length; i<len; i++) {
+      let key = localStorage.key(i)
+      let value = localStorage[key]
+      if(value === inputVal){
+        console.log(key + " => " + value)
+        localStorage.removeItem(key)
+      }
+    }
+   }
 
   $('.form').on('click', '.add-task', function() {
       let inputTask = $('<input type="text" class="input-task" placeholder="Enter your task">')
@@ -23,16 +45,21 @@ $(document).ready(function(){
 
   $('.form').on('click', '.input-save', function() {
     let inputVal = $('.input-task').val()
-    inputVal ? addTaskItem() : alert('You really enter your task?')
+    inputVal ? (
+      addTaskItem(inputVal),
+      addLocalStorage(inputVal)
+    ): alert('You really enter your task?')
     reset()
   })
 
   $('.task-list').on('click', '.input-delete', function() {
    $(this).parent().slideUp(2000)
+    let inputVal = $(this).parent().find('.item-task').val()
+    removeLocalStorage(inputVal)
   })
 
   $('.task-list').on('click', '.input-edit', function() {
-    let oldValue = $(this).parent().find('.item-task').val()
+    oldValue = $(this).parent().find('.item-task').val()
     let inputTaskEdit = $(this).parent().find('.item-task')
     inputTaskEdit.addClass('editItem')
     let editValue = inputTaskEdit.val()
@@ -51,7 +78,8 @@ $(document).ready(function(){
   })
 
   $('.task-list').on('click', '.input-cancel', function() {
-    let taskItem = $(`<li><input type="text" class="item-task" value=${oldVal}>
+    console.log(oldValue);
+    let taskItem = $(`<li><input type="text" class="item-task" value=${oldValue}>
     <input type="button" class="input-delete" value="delete">
     <input type="button" class="input-edit" value="edit"></li>`)
     $(this).parent().empty().append(taskItem)
